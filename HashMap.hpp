73ded55160f61dfea3acc80d99a24891fb6c7747
swish0621 <swish0621@gmail.com>
2025-10-14 14:32:14 -0600

    add HashMap insert and hash function

--- HashMap.hpp
+++ HashMap.hpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <functional>
 
-template<typename T, typename U>
+template<typename T>
 class HashMap {
     public:
-        HashMap(std::vector<T> cals, inst size){}
-        int hashKey(T val){}
-        void insertElement(T val){}
+        HashMap(std::vector<T> cals, int size){}
+
+        // Input: a value to be put into the hashtable
+        // Result: an integer with the bucket index 
+        int hashKey(T val){
+            std::hash<T> func;
+            std::size_t index = func(val);
+            return index;
+        }
+
+        // Input: value to be inserted
+        // Result: calls the hash function to get the index to insert the value into
+        // if bucket reaches max size the hashtable is expanded and values are redistributed 
+        void insertElement(T val){
+            int key = hashKey(val);
+            size_t size = store[key].size();
+            if(size > max_size_){
+                resize();
+                key = hashKey(val);
+            }
+            store[key].push_back(val);
+        }
+
         bool search(T val){}
         void deleteElement(T val){}
-        std::ostream& operator<<(std::ostream& os, const HashMap<U> &m){}
+        void resize(){}
+
+        // template<typename U>
+        // friend std::ostream& operator<<(std::ostream& os, const HashMap<U> &m);
+
+        private:
+            int num_buckets_;
+            std::vector<std::vector<T>> store_;
+            const int max_size = 10;
 };
